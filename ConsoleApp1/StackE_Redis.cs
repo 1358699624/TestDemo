6fe6030e1b9e8cd71a7032b8a3d7ebbe82f7a93c
马 <407262285@qq.com>
2024-08-28 16:21:18 +0800

    添加项目文件。

---
+++ ConsoleApp1/StackE_Redis.cs
@@ -0,0 +1,162 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class StackE_Redis
+    {
+
+        // 配置连接字符串
+       static string redisConnectionString = "localhost:6379";
+
+
+        public  static  void Read() 
+        {
+
+            // 创建连接
+            using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(redisConnectionString))
+            {
+                // 获取数据库
+                IDatabase db = redis.GetDatabase();
+
+                // 设置键值对
+                db.StringSet("myKey", "myValue");
+
+                // 获取值,string
+                string value = db.StringGet("myKey");
+                Console.WriteLine($"The value of 'myKey' is: {value}");
+
+                Console.WriteLine("--------------------------------------------");
+
+                //List  类型
+                RedisValue[] arr = new RedisValue[10];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = new RedisValue($"测试Redis{i.ToString()}");
+                    Console.WriteLine($"测试Redis{i.ToString()}");
+                }
+
+                db.ListLeftPush("Listtest", arr);
+
+                RedisValue myKey = db.StringGet("myKey");
+                 
+                RedisValue[] list = db.ListRange("Listtest", 0, 5);
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("--------------------------------------------list");
+
+                //redis HashSet
+
+                db.KeyDelete("myHashSet");
+                db.HashSet("myHashSet", "id", "9527");
+                db.HashSet("myHashSet", "userName", "admin");
+                db.HashSet("myHashSet", "sex", "男");
+                db.HashSet("myHashSet", "age", "22");
+                db.HashDelete("myHashSet", "sex");
+                HashEntry[] hashEntries = db.HashGetAll("myHashSet");
+                foreach (var item in hashEntries)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("--------------------------------------------hashset");
+                //set
+                db.KeyDelete("mySet");
+                db.SetAdd("mySet", "a1");
+                db.SetAdd("mySet", "a2");
+                db.SetAdd("mySet", "a3");
+                db.SetAdd("mySet", "a4");
+                db.SetAdd("mySet", "a5");
+                RedisValue[] redisValues = db.SetMembers("mySet");
+                foreach (var item in redisValues)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("--------------------------------------------set");
+
+                //set 交集
+              
+                db.SetAdd("mySet4", "a1");
+                db.SetAdd("mySet4", "a2");
+                db.SetAdd("mySet4", "a6");
+                db.SetAdd("mySet4", "a7");
+                RedisValue[] redisValue2 = db.SetCombine(SetOperation.Intersect, "mySet", "mySet4");
+                foreach (var item in redisValue2)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine("--------------------------------------------交集");
+
+
+
+
+
+                //差集
+                RedisValue[] redisValues6 = db.SetCombine(SetOperation.Difference, "mySet", "mySet4");
+                foreach (var item in redisValues6)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("--------------------------------------------差集");
+
+
+
+
+                //UNION 并集
+                RedisValue[] redisValues5 = db.SetCombine(SetOperation.Union, "mySet", "mySet4");
+                foreach (var item in redisValues5)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("--------------------------------------------并集");
+
+
+                db.KeyDelete("myZset");
+                db.SortedSetAdd("myZset", "a1", 69);
+                db.SortedSetAdd("myZset", "a2", 79);
+                db.SortedSetAdd("myZset", "a3", 85);
+                db.SortedSetAdd("myZset", "a4", 82);
+                db.SortedSetAdd("myZset", "a5", 92);
+                //分数范围
+                RedisValue[] redisValues_zset = db.SortedSetRangeByScore("myZset", 70, 90);
+                foreach (var item in redisValues_zset)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("--------------------------------------------ZSET");
+
+
+                //正序
+                SortedSetEntry[] sortedSetEntries = db.SortedSetRangeByRankWithScores("myZset", 0, -1, Order.Ascending);
+                foreach (var item in sortedSetEntries)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine("--------------------------------------------ZSET");
+
+
+
+                //倒叙
+                SortedSetEntry[] sortedSetEntries_desc = db.SortedSetRangeByRankWithScores("myZset", 0, -1, Order.Descending);
+                foreach (var item in sortedSetEntries_desc)
+                {
+                    Console.WriteLine(item);
+                }
+
+
+                redis.Close();
+
+            }
+
+        }
+
+    }
+}
---
+++ TestDemo/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace TestDemo
+{
+    internal class Program
+    {
+        [Obsolete]
+        static void Main(string[] args)
+        {
+
+
+            string originalString = "0Hello";
+
+            // 如果字符串的首字母是'0'，则去掉它  
+            if (originalString.Length > 0 && originalString[0] == '0')
+            {
+                originalString = originalString.Substring(1);
+            }
+
+            Console.WriteLine(originalString); // 输出 "Hello"  
+
+            string start = "2024-07-24";
+            string end = "2024-07-25";
+            TimeSpan timeSpan =  Convert.ToDateTime(end) - Convert.ToDateTime(start);
+            Console.WriteLine(timeSpan.Days);
+
+            //string time = "2019-05-08T11:37:42.5942115Z";
+            //// 转成UTC时间
+            //DateTime formatStartTime = DateTime.Parse(time);
+            //Console.WriteLine(formatStartTime);
+
+
+            //double doubleValue = 3.141592653589793;
+            //float floatValue = (float)doubleValue;
+            //Console.WriteLine(doubleValue);
+            //Console.WriteLine(floatValue);
+
+
+
+            byte[] data = new byte[] { 0x55, 0xAA, 0x00, 0x01, 0x55, 0xBB, 0xFF, 0xFE, 0x55, 0xAA, 0xAB, 0xCD, 0x55, 0xBB, 0xEF, 0x12 };
+
+            List<byte[]> segments = ExtractSegments(data, new byte[] { 0x55, 0xAA }, new byte[] { 0x55, 0xBB });
+
+            foreach (var segment in segments)
+            {
+                Console.WriteLine($"Segment: {BitConverter.ToString(segment)}");
+            }
+            Console.ReadLine();
+
+
+
+            return;
+
+        }
+
+
+        static List<byte[]> ExtractSegments(byte[] data, params byte[][] patterns)
+        {
+            List<byte[]> segments = new List<byte[]>();
+
+            int startIndex = 0;
+
+            while (startIndex < data.Length)
+            {
+                foreach (var pattern in patterns)
+                {
+                    if (startIndex + pattern.Length <= data.Length && IsMatch(data, startIndex, pattern))
+                    {
+                        segments.Add(pattern);
+                        startIndex += pattern.Length;
+                        break;
+                    }
+                }
+
+                // 如果当前位置没有匹配任何模式，则向前移动一位  
+                if (startIndex == data.Length || !patterns.Any(p => startIndex + p.Length <= data.Length && IsMatch(data, startIndex, p)))
+                {
+                    startIndex++;
+                }
+            }
+
+            return segments;
+        }
+
+        static bool IsMatch(byte[] data, int startIndex, byte[] pattern)
+        {
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (data[startIndex + i] != pattern[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /**/
+        /// <summary>
+        /// 将Xml内容字符串转换成DataSet对象
+        /// </summary>
+        /// <param name="xmlStr">Xml内容字符串</param>
+        /// <returns>DataSet对象</returns>
+        private static DataSet CXmlToDataSet(string xmlStr)
+        {
+            if (!string.IsNullOrEmpty(xmlStr))
+            {
+                StringReader StrStream = null;
+                XmlTextReader Xmlrdr = null;
+                try
+                {
+                    DataSet ds = new DataSet();
+                    //读取字符串中的信息
+                    StrStream = new StringReader(xmlStr);
+                    //获取StrStream中的数据
+                    Xmlrdr = new XmlTextReader(StrStream);
+                    //ds获取Xmlrdr中的数据                
+                    ds.ReadXml(Xmlrdr);
+                    return ds;
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                finally
+                {
+                    //释放资源
+                    if (Xmlrdr != null)
+                    {
+                        Xmlrdr.Close();
+                        StrStream.Close();
+                        StrStream.Dispose();
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        private static DataTable GetOperDataByRtID(string xml, string atID)
+        {
+            DataSet ds = new DataSet();
+            TextReader tr = new StringReader(xml);
+            ds.ReadXml(tr);
+            if (ds.Tables.Count > 0)
+            {
+                DataRow[] dsOper = ds.Tables["inspectionResult"].Select("inspectionResultList_ID='" + atID + "'");
+                DataTable dtoperAt = ds.Tables["inspectionResult"].Clone();
+                foreach (DataRow dr in dsOper)
+                {
+                    dtoperAt.Rows.Add(dr.ItemArray);
+                }
+                return dtoperAt;
+            }
+            return null;
+        }
+        private static DataTable GetOperDataByRtID2(string xml, string atID)
+        {
+            DataSet ds = new DataSet();
+            TextReader tr = new StringReader(xml);
+            ds.ReadXml(tr);
+            if (ds.Tables.Count > 0)
+            {
+                DataRow[] dsOper1 = ds.Tables["clinicalProjectList"].Select("inspectionApply_ID='" + atID + "'");
+                DataTable dtoperAt1 = ds.Tables["clinicalProjectList"].Clone();
+                foreach (DataRow dr in dsOper1)
+                {
+                    dtoperAt1.Rows.Add(dr.ItemArray);
+                }
+                if (dtoperAt1.Rows.Count <= 0)
+                {
+                    return null;
+                }
+                DataTable dtoperAt2 = null;
+                foreach (DataRow item in dtoperAt1.Rows)
+                {
+                    DataRow[] dsOper2 = ds.Tables["clinicalProject"].Select("clinicalProjectList_id='" + item["clinicalProjectList_id"] + "'");
+                    dtoperAt2 = ds.Tables["clinicalProject"].Clone();
+                    foreach (DataRow dr in dsOper2)
+                    {
+                        dtoperAt2.Rows.Add(dr.ItemArray);
+                    }
+                }
+                return dtoperAt2;
+            }
+            return null;
+        }
+    }
+}
